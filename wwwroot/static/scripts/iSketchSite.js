021e8057b48593ef29c65a548efd5f113f7abd88
Dylan Bickerstaff <dylan@belowaverage.org>
2021-06-17 14:40:35 -0400

    Loading icon on buttons.

--- wwwroot/static/scripts/iSketchSite.js
+++ wwwroot/static/scripts/iSketchSite.js
@@ -1,1 +1,8 @@
-﻿
+﻿document.body.addEventListener('click', function (e) {
+    e.path.forEach(function (t) {
+        if (t.classList != undefined && t.classList.contains('CButton')) {
+            t.classList.add('clicked');
+            return true;
+        }
+    });
+});
