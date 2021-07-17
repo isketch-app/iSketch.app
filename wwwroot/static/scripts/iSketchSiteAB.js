b8dc476f3ae3c881162709644f992877ebdc2d0b
Dylan Bickerstaff <dylan@belowaverage.org>
2021-07-17 13:21:50 -0400

    Update iSketchSiteAB.js

--- wwwroot/static/scripts/iSketchSiteAB.js
+++ wwwroot/static/scripts/iSketchSiteAB.js
@@ -1,10 +1,12 @@
-﻿Blazor.defaultReconnectionHandler.defaultOnConnectionDown = Blazor.defaultReconnectionHandler.onConnectionDown;
+iSketchSite.Blazor.reconnectHandlerFailedRejected = function(e = null) {
+    location.reload();
+};
+Blazor.defaultReconnectionHandler.defaultOnConnectionDown = Blazor.defaultReconnectionHandler.onConnectionDown;
 Blazor.defaultReconnectionHandler.onConnectionDown = function (opts, message) {
     opts.maxRetries = 3;
     opts.retryIntervalMilliseconds = 1000;
     Blazor.defaultReconnectionHandler.defaultOnConnectionDown(opts, message);
     Blazor.defaultReconnectionHandler._reconnectionDisplay.message.innerText = "Connection lost, reconnecting...";
-    Blazor.defaultReconnectionHandler._reconnectionDisplay.failed = function (e) {
-        location.reload();
-    }
+    Blazor.defaultReconnectionHandler._reconnectionDisplay.failed = iSketchSite.Blazor.reconnectHandlerFailedRejected;
+    Blazor.defaultReconnectionHandler._reconnectionDisplay.rejected = iSketchSite.Blazor.reconnectHandlerFailedRejected;
 };
