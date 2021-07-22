128ebf40a52b5264fc6f208d3202e855d4f134de
Dylan Bickerstaff <dylan@belowaverage.org>
2021-07-22 15:07:13 -0400

    Test Authorization.

---
+++ Data/Authentication/iSketchAuthenticationHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace iSketch.app.Data.Authentication
+{
+    public class iSketchAuthenticationHandler : IAuthenticationHandler
+    {
+        public Task<AuthenticateResult> AuthenticateAsync()
+        {
+            return Task.FromResult(AuthenticateResult.Fail(":("));
+            //throw new NotImplementedException();
+        }
+
+        public Task ChallengeAsync(AuthenticationProperties properties)
+        {
+            return Task.CompletedTask;
+            //throw new NotImplementedException();
+        }
+
+        public Task ForbidAsync(AuthenticationProperties properties)
+        {
+            return Task.CompletedTask;
+            //throw new NotImplementedException();
+        }
+
+        public Task InitializeAsync(AuthenticationScheme scheme, HttpContext context)
+        {
+            return Task.CompletedTask;
+            //throw new NotImplementedException();
+        }
+    }
+}
