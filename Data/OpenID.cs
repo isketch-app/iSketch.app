e60ead7efefefc76be1492d171841395d1ac5dd3
Dylan Bickerstaff <dylan@belowaverage.org>
2021-09-05 18:50:43 -0400

    Push for now.

--- Data/OpenID.cs
+++ Data/OpenID.cs
@@ -6,6 +6,11 @@ using System.Collections.Generic;
 using System.Web;
 using System;
 using System.Data.SqlClient;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text;
 
 namespace iSketch.app.OpenID
 {
@@ -74,6 +79,10 @@ namespace iSketch.app.OpenID
                 if (!rdr.IsDBNull(8)) idp.ClaimsEmail = rdr.GetString(8);
                 if (!rdr.IsDBNull(9)) idp.ClaimsUserPhoto = rdr.GetString(9);
             }
+            catch (Exception)
+            {
+                idp = null;
+            }
             finally
             {
                 rdr.Close();
@@ -108,17 +117,99 @@ namespace iSketch.app.OpenID
             HttpUtility.UrlEncode(GetRedirectURI(session));
         }
     }
+    public class Token
+    {
+        [JsonPropertyName("id_token")]
+        public string IDToken { get; set; }
+    }
+    public class JWT
+    {
+        public Dictionary<string, object> Header;
+        public Dictionary<string, object> Payload;
+        public byte[] Signature;
+        public JWT(string RawToken)
+        {
+            try
+            {
+                string rJsonHeader;
+                string rJsonPayload;
+                string[] splitToken = RawToken.Split('.');
+                rJsonHeader = Encoding.Default.GetString(ConvertFromB64Url(splitToken[0]));
+                rJsonPayload = Encoding.Default.GetString(ConvertFromB64Url(splitToken[1]));
+                Header = JsonSerializer.Deserialize<Dictionary<string, object>>(rJsonHeader);
+                Payload = JsonSerializer.Deserialize<Dictionary<string, object>>(rJsonPayload);
+                Signature = ConvertFromB64Url(splitToken[2]);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Failed to de-serialize the JsonWebToken.");
+            }
+        }
+        public static byte[] ConvertFromB64Url(string str)
+        {
+            str = str.Replace('-', '+');
+            str = str.Replace('_', '/');
+            int padding = str.Length % 4;
+            if (padding == 3) str += "=";
+            if (padding == 2) str += "==";
+            return Convert.FromBase64String(str);
+        }
+    }
     public static class Endpoints
     {
         public static async Task Login(HttpContext con)
         {
             Session session = con.InitializeSession();
+            idP idP = OpenID.GetIDP(session.db, Guid.Parse(con.Request.RouteValues["IdpID"].ToString()));
+            if (idP == null)
+            {
+                con.Response.Redirect("/error/openid/idp-does-not-exist");
+                return;
+            }
+            if (!con.Request.Query.ContainsKey("code") || con.Request.Query["code"] == "")
+            {
+                con.Response.Redirect("/error/openid/code-missing");
+                return;
+            }
+            string code = con.Request.Query["code"];
 
-            idP asdf = OpenID.GetIDP(session.db, Guid.Parse(con.Request.RouteValues["IdpID"].ToString()));
+            HttpClient hc = new();
+            HttpRequestMessage msg = new();
+            msg.RequestUri = new(idP.EndpointToken);
+            msg.Method = HttpMethod.Post;
+            FormUrlEncodedContent form = new(new Dictionary<string, string>() {
+                { "grant_type", "authorization_code" },
+                { "code", code },
+                { "redirect_uri", idP.GetRedirectURI(session) },
+                { "client_id", idP.ClientID }
+            });
+            msg.Content = new StreamContent(await form.ReadAsStreamAsync());
+            msg.Content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            HttpResponseMessage response = await hc.SendAsync(msg);
+            Token Token = await response.Content.ReadFromJsonAsync<Token>();
+            if (Token.IDToken == null)
+            {
+                con.Response.Redirect("/error/openid/jwt-missing");
+                return;
+            }
+            JWT JWT;
+            try
+            {
+                JWT = new(Token.IDToken);
+            }
+            catch (Exception)
+            {
+                con.Response.Redirect("/error/openid/jwt-invalid");
+                return;
+            }
+            
+            foreach (KeyValuePair<string, object> claim in JWT.Payload)
+            {
+                await con.Response.WriteAsync(claim.Key + ": " + claim.Value.ToString() + "\r\n");
+            }
 
-            await con.Response.WriteAsync(asdf.GetRequestURI(session) + "\r\n\r\n");
-            await con.Response.WriteAsync(con.Request.Query["code"]);
-
+            //con.Response.Redirect("/");
+            await Task.CompletedTask;
         }
     }
 
