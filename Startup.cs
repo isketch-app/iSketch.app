07c5be95cc1be0d4a17659719a2fd0a54e99d30e
Dylan Bickerstaff <dylan@belowaverage.org>
2022-02-03 23:33:32 -0500

    .

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.SignalR;
 using iSketch.app.Services;
 using iSketch.app.Data.Middleware;
 using iSketch.app.Data.Photo;
